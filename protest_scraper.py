0824eb5b36749c8783b86569c35167f8e15ae00a
azr98 <52422859+azr98@users.noreply.github.com>
2020-04-12 17:53:40 +0100

    Got td list of dates

--- protest_scraper.py
+++ protest_scraper.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
-my_url = "https://www.glasgow.gov.uk/futureprocessions?fPst=1"
+my_url = "https://www.glasgow.gov.uk/futureprocessions"
 uClient = uReq(my_url)
 page_of_protests = uClient.read()
 
 # html parser
-page_soup = soup(page_of_protests, "html.parser")
+soup = soup(page_of_protests, "html.parser")
 
+dates = soup.findAll("td", {"class": "DiaryDayHeadingStyle"})
+print(dates)
